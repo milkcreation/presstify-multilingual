8720d40cb1479a8f51784f6e283d979d4a482c88
Jordy Manner <jordy.manner@milkcreation.fr>
2019-07-08 07:04:12 +0000

    multilingual -- branch 2.0 -- Ajustement maj tiFy

---
+++ Resources/assets/partial/multilingual-dropdown/index.js
@@ -0,0 +1,9 @@
+/** DEPS **/
+/** tiFy - dropdown */
+import 'presstify-framework/partial/dropdown/index';
+
+/** multilingual flag */
+import '../multilingual-flag/index';
+
+/** Sass */
+import './css/styles.css';
